809631be5372833f5c5bb5d62f8a11315c97e29a
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-10-11 21:26:03 +0200

    [Add] Load_image works
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,20 +1,13 @@
 #include "utils.h"
 
+SDL_Surface* load_image(const char* path)
+{
+    SDL_Surface* s = IMG_Load(path);
+    if (s == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
 
-// Loads an image in a surface.
-// The format of the surface is SDL_PIXELFORMAT_RGB888.
-//
-// path: Path of the image.
-SDL_Surface *load_image(const char *path) {
-  // !TODO
-  //  SDL_Surface *surface = IMG_Load(path);
-  //  SDL_Surface *converted =
-  //      SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGB888, 0);
-  //  SDL_FreeSurface(surface);
-  //
-  //  if (converted == NULL)
-  //    errx(1, "Could not load the image: %s", IMG_GetError());
-  //
-  //  return converted;
-  return NULL;
+    SDL_Surface *res = SDL_ConvertSurfaceFormat(s, SDL_PIXELFORMAT_RGB888, 0);
+    SDL_FreeSurface(s);
+
+    return res;
 }
