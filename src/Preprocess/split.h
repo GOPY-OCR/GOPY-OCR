d968ee2fb2f0d0358274eb521f686a777f23b0cb
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-10-11 19:41:08 +0200

    layout of the code
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Preprocess/split.h
+++ src/Preprocess/split.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-SDL_Surface* crop_surface(const SDL_Surface* grid, int x, int y, int width, int height);
+SDL_Surface* crop_surface(const SDL_Surface* grid, int x, int y, 
+                          int width, int height);
 SDL_Surface*[] split_sudoku(const SDL_Surface* grid);
 
 #endif
