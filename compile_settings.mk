e202bf470f46e99fe4459e5910c20aaa811facf4
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-10-16 12:20:43 +0200

    Complete clean rule for the main Makefile
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- compile_settings.mk
+++ compile_settings.mk
@@ -7,11 +7,9 @@ USED_LIBS = sdl2 SDL2_image
 # Flags (with = and not := so flags can be added after)
 CFLAGS = -Wall -Wextra `pkg-config --cflags $(USED_LIBS)` $(OPTFLAGS)
 OPTFLAGS := -O3
-CPPFLAGS = -MMD $(INCLUDE_DIRS)
+CPPFLAGS = -MMD $(addprefix -I, $(INCLUDE_DIRS))
 LDFLAGS :=
 LDLIBS := `pkg-config --libs $(USED_LIBS)` -lm
-INCLUDE_DIRS = $(addprefix -I, \
-			   src/Utils)
 
 # Debug mode can be enabled by executing `make DEBUG=1 rule_name`
 DEBUG := 0
