063f8860c2ec3c4545a5dd160b5859b72488864d
oscar <oscar.le-dauphin@epita.fr>
2022-10-17 20:58:40 +0200

    started grid_detection
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

---
+++ src/Preprocess/grid_detection.c
@@ -0,0 +1,8 @@
+#include "grid_detection.h"
+
+void grid_detection(SDL_Surface *image, int *x1, int *y1, int *x2, int *y2){
+    *x1 = 0;
+    *y1 = 0;
+    *x2 = 0;
+    *y2 = 0;
+}
---
+++ src/Preprocess/grid_detection.h
@@ -0,0 +1,6 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "utils.h"
+
+
+void grid_detection(SDL_Surface *image, int *x1, int *y1, int *x2, int *y2);
