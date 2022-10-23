e4f9398361a765c55af6986084a135320bff0909
oscar <oscar.le-dauphin@epita.fr>
2022-10-23 02:16:00 +0200

    removed principal component analysis functions, too complicated
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- src/Preprocess/grid_detection.h
+++ src/Preprocess/grid_detection.h
@@ -1,22 +1,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "utils.h"
-#include "queue.h"
-
 
 void grid_detection(SDL_Surface *image, int *x1, int *y1, int *x2, int *y2);
-
-// Returns an image containing only
-// the largest shape of connected pixels
-// in the input image
-#define MAX_COMPONENTS 100
-SDL_Surface *principal_component_analysis(SDL_Surface *image, int connectivity);
-
-// Returns true if the pixel at (x, y) is connected
-// to a component via a path in the image
-// 
-// connectivity: number of pixels to check around the pixel
-int is_connected(SDL_Surface *component, SDL_Surface *image, int x, int y, int connectivity);
 
 int *project(SDL_Surface *image, int axis);
 
