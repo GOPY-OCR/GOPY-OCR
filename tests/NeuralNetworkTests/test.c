cf9881cae7294878393a33625bd0a3cf92e23130
oscar <oscar.le-dauphin@epita.fr>
2022-10-23 16:28:20 +0200

    moved matrices tests to UtilsTests/
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- tests/NeuralNetworkTests/test.c
+++ tests/NeuralNetworkTests/test.c
@@ -1,3 +1,2 @@
 #include <criterion/criterion.h>
-#include "test_matrices.c"
 #include "test_neural_network.c"
--- tests/UtilsTests/test.c
+++ tests/UtilsTests/test.c
@@ -1,2 +1,3 @@
 #include <criterion/criterion.h>
 #include "test_utils.c"
+#include "test_matrices.c"
