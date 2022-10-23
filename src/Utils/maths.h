2ae4fd1a9e8afd9c519118d83398d653a6204ef7
oscar <oscar.le-dauphin@epita.fr>
2022-10-23 15:23:09 +0200

    moved maths to utils, and added degrees_to_radian, cos_degree and sin_degree
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- src/Utils/maths.h
+++ src/Utils/maths.h
@@ -4,3 +4,8 @@
 double sigmoid(double x);
 double sigmoid_prime(double x);
 double doubleabs(double x);
+
+double degree_to_radian(double degree);
+
+double cos_degree(double degree);
+double sin_degree(double degree);
