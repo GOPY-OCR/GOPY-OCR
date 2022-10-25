fd903458b4aa4b25aad910553ef87d2a17371e92
Pierre Wagniart <pierre.wagniart@epita.fr>
2022-10-25 02:26:58 +0200

    Finished but untested version of rotation fonction

--- src/Preprocess/ManualRot.c
+++ src/Preprocess/ManualRot.c
@@ -48,4 +48,94 @@ void manrot(char[] file , int degree)
 			bmBilinearInterpolation.SetPixel(j, i, Color.Black);
 		}
 	}
-				
+	// assigning pixels of destination image from source image
+	// with bilinear interpolation
+	for (i = 0; i < iHeight; ++i)
+	{
+		for (j = 0; j < iWidth; ++j)
+		{
+			// convert raster to Cartesian
+			x = j - iCentreX;
+			y = iCentreY - i;
+
+			// convert Cartesian to polar
+			fDistance = Math.Sqrt(x * x + y * y);
+			fPolarAngle = 0.0;
+			if (x == 0)
+			{
+				if (y == 0)
+				{
+					// centre of image, no rotation needed
+					bmBilinearInterpolation.SetPixel(j, i, bm.GetPixel(j, i));
+					continue;
+				}
+				else if (y < 0)
+				{
+					fPolarAngle = 1.5 * Math.PI;
+				}
+				else
+				{
+					fPolarAngle = 0.5 * Math.PI;
+				}
+			}
+			else
+			{
+				fPolarAngle = Math.Atan2((double)y, (double)x);
+			}
+
+			// the crucial rotation part
+			// "reverse" rotate, so minus instead of plus
+			fPolarAngle -= cnAngle;
+
+			// convert polar to Cartesian
+			fTrueX = fDistance * Math.Cos(fPolarAngle);
+			fTrueY = fDistance * Math.Sin(fPolarAngle);
+
+			// convert Cartesian to raster
+			fTrueX = fTrueX + (double)iCentreX;
+			fTrueY = (double)iCentreY - fTrueY;
+
+			iFloorX = (int)(Math.Floor(fTrueX));
+			iFloorY = (int)(Math.Floor(fTrueY));
+			iCeilingX = (int)(Math.Ceiling(fTrueX));
+			iCeilingY = (int)(Math.Ceiling(fTrueY));
+
+			// check bounds
+			if (iFloorX < 0 || iCeilingX < 0 || iFloorX >= iWidth || iCeilingX >= iWidth || iFloorY < 0 || iCeilingY < 0 || iFloorY >= iHeight || iCeilingY >= iHeight) continue;
+
+			fDeltaX = fTrueX - (double)iFloorX;
+			fDeltaY = fTrueY - (double)iFloorY;
+
+			clrTopLeft = bm.GetPixel(iFloorX, iFloorY);
+			clrTopRight = bm.GetPixel(iCeilingX, iFloorY);
+			clrBottomLeft = bm.GetPixel(iFloorX, iCeilingY);
+			clrBottomRight = bm.GetPixel(iCeilingX, iCeilingY);
+
+			// linearly interpolate horizontally between top neighbours
+			fTopRed = (1 - fDeltaX) * clrTopLeft.R + fDeltaX * clrTopRight.R;
+			fTopGreen = (1 - fDeltaX) * clrTopLeft.G + fDeltaX * clrTopRight.G;
+			fTopBlue = (1 - fDeltaX) * clrTopLeft.B + fDeltaX * clrTopRight.B;
+
+			// linearly interpolate horizontally between bottom neighbours
+			fBottomRed = (1 - fDeltaX) * clrBottomLeft.R + fDeltaX * clrBottomRight.R;
+			fBottomGreen = (1 - fDeltaX) * clrBottomLeft.G + fDeltaX * clrBottomRight.G;
+			fBottomBlue = (1 - fDeltaX) * clrBottomLeft.B + fDeltaX * clrBottomRight.B;
+
+			// linearly interpolate vertically between top and bottom interpolated results
+			iRed = (int)(Math.Round((1 - fDeltaY) * fTopRed + fDeltaY * fBottomRed));
+			iGreen = (int)(Math.Round((1 - fDeltaY) * fTopGreen + fDeltaY * fBottomGreen));
+			iBlue = (int)(Math.Round((1 - fDeltaY) * fTopBlue + fDeltaY * fBottomBlue));
+
+			// make sure colour values are valid
+			if (iRed < 0) iRed = 0;
+			if (iRed > 255) iRed = 255;
+			if (iGreen < 0) iGreen = 0;
+			if (iGreen > 255) iGreen = 255;
+			if (iBlue < 0) iBlue = 0;
+			if (iBlue > 255) iBlue = 255;
+
+			bmBilinearInterpolation.SetPixel(j, i, Color.FromArgb(iRed, iGreen, iBlue));
+		}
+	}
+	bmBilinearInterpolation.Save("rotationbilinearinterpolation.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
