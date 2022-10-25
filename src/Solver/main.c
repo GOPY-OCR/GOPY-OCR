89d7fc7764c72dc74d8c35a2f32d6935ea380b1a
YnessK <97630948+YnessK@users.noreply.github.com>
2022-10-25 21:53:47 +0200

    Update main.c
    main.c solver

--- src/Solver/main.c
+++ src/Solver/main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
+#include <string.h>
+#include "solver.c"
+#include "grid.c"
 
-int main(int argc, char **argv) { return 0; }
+
+int main(int argc, char **argv) 
+{ 
+    if (argc != 1){
+        err(1);
+    }
+
+    else{
+        int grid[81] = {0};
+        char file[strlen(file) + 7] = argv[1];
+        load_grid_file(file, grid);
+        Solve();
+        strcat(file, ".result");
+        save_grid_file(grid, file);
+    }
+
+    return 0; 
+}
