427fee448df0c7d10999d2c7fabe867c7862138d
oscar <oscar.le-dauphin@epita.fr>
2022-10-25 14:55:55 +0200

    removed unused grid_detection functions, I gave up on this technique
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- src/Preprocess/grid_detection.h
+++ src/Preprocess/grid_detection.h
@@ -6,15 +6,3 @@
 #include "pixel_utils.h"
 
 void grid_detection(SDL_Surface *image, int *x1, int *y1, int *x2, int *y2);
-
-int *project(SDL_Surface *image, int axis);
-
-// aka écart type
-float standard_deviation(int *xs, int size);
-
-// image: max convex component output
-// returns the angle at which the image is rotated
-// in degrees
-int automatic_image_rotation(SDL_Surface *image);
-
-float evaluate_rotation(SDL_Surface *image, int angle);
