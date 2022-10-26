1b09e1b9c18f8d991c11236f4765f33fafac404a
oscar <oscar.le-dauphin@epita.fr>
2022-10-26 16:53:42 +0200

    adapted tests to pngs
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- tests/UtilsTests/test_utils.c
+++ tests/UtilsTests/test_utils.c
@@ -7,7 +7,7 @@
 Test(utils, test_load_image) {
     for (int i = 1; i <= CDC_NUM_TESTS; i++) {
         char *filename = calloc(100, sizeof(char));
-        sprintf(filename, "images/cdc/%d.jpeg", i);
+        sprintf(filename, "images/cdc/%d.png", i);
         SDL_Surface *im = load_image(filename);
         
         cr_assert(im != NULL, "Image %d is NULL", i);
@@ -25,7 +25,7 @@ Test(utils, test_load_image) {
 Test(utils, test_save_image) {
     for (int i = 1; i <= CDC_NUM_TESTS; i++) {
         char *filename = calloc(100, sizeof(char));
-        sprintf(filename, "images/cdc/%d.jpeg", i);
+        sprintf(filename, "images/cdc/%d.png", i);
         SDL_Surface *im = load_image(filename);
 
         sprintf(filename, "../_build/tests/UtilsTests/TEST_utils_cdc_%d.png", i);
