51dfd070203c19f0648b72d38dd70d3a019d5053
oscar <oscar.le-dauphin@epita.fr>
2022-10-26 19:49:24 +0200

    demo of the latest grid_detection
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- src/Preprocess/main.c
+++ src/Preprocess/main.c
@@ -1,15 +1,13 @@
 #include "utils.h"
 #include "hough_transform.h"
 #include "flood_fill.h"
+#include "debug_utils.h"
 #include <SDL2/SDL.h>
-
 
 int main(int argc, char **argv) {
     SDL_Surface *image = load_image(argv[1]);
 
-    Rect grid = grid_detection(image, 1);
-
-    printf("Grid detected at ((%d, %d), (%d, %d))\n", grid.p1.x, grid.p1.y, grid.p2.x, grid.p2.y);
+    Quad grid = grid_detection(image, 1);
 
     save_image(image, "output.png");
 
