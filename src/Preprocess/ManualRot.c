b9f9f0b124af40608ed80b196e26154a3f12ed10
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-10-27 12:32:14 +0200

    Add .h file
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Preprocess/ManualRot.c
+++ src/Preprocess/ManualRot.c
@@ -1,6 +1,5 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL2_gfxPrimitives.h>
-#include <SDL2/SDL2_rotozoom.h>
+#include "ManualRot.h"
+
 void SimpleRot(SDL_Surface* image, double angle)
 {
  rotozoomSurface(image, angle,1.0,1);
---
+++ src/Preprocess/ManualRot.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL2_rotozoom.h>
+
+void SimpleRot(SDL_Surface* image, double angle);
+
