3ecf5cb782a844e7db0e478fddd6b0cf9575883f
Pierre Wagniart <pierre.wagniart@epita.fr>
2022-10-27 14:14:18 +0200

    Particulerement desagreable

--- src/Preprocess/ManualRot.c
+++ src/Preprocess/ManualRot.c
@@ -1,10 +1,7 @@
 #include <SDL2/SDL.h>
-#include "SDL/SDL_image.h"
-#include "SDL/SDL_rotozoom.h"
-#include <SDL2/SDL_GFX.h>
-
-SDL_Surface SimpleRot(SDL_Surface *image, double angle)
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL2_rotozoom.h>
+void SimpleRot(SDL_Surface* image, double angle)
 {
-  SDL_Surface end = rotozoomSurface(*image, angle,1.0,1);
-  return end;
+ rotozoomSurface(image, angle,1.0,1);
 }
