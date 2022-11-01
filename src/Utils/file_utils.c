1bdfe411b234fee46fc55ebc48cdcdd1202d22a4
oscar <oscar.le-dauphin@epita.fr>
2022-11-01 14:52:25 +0100

    moved list_file function to file_utils, and created new function count_file_in_dir
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- src/Utils/file_utils.c
+++ src/Utils/file_utils.c
@@ -4,3 +4,59 @@
 int file_exists(const char *filename){
     return access(filename, F_OK) == 0;
 }
+
+
+char **list_files(const char *path, const int n, int append_path) {
+    DIR *dir;
+    struct dirent *ent;
+    char **files = malloc(n * sizeof(char *));
+    int i = 0;
+
+    int pathle = strlen(path);
+    pathle -= pathle > 0 && path[pathle - 1] == '/'; // ignore trailing slash
+
+    if ((dir = opendir(path)) != NULL) {
+        while ((ent = readdir(dir)) != NULL && i < n) {
+            if (ent->d_type == DT_REG) {
+                if (append_path) {
+                    int filele = strlen(ent->d_name);
+                    files[i] = malloc(pathle + filele + 2);
+                    strcpy(files[i], path);
+                    files[i][pathle] = '/';
+                    strcpy(files[i] + pathle + 1, ent->d_name);
+                } else {
+                    files[i] = malloc(strlen(ent->d_name) + 1);
+                    strcpy(files[i], ent->d_name);
+                }
+                if (files[i] != NULL) {
+                    i++;
+                }
+            }
+        }
+        closedir(dir);
+    } else {
+        errx(1, "list_files: Could not open directory %s", path);
+    }
+
+    return files;
+}
+
+
+int count_files_in_dir(const char *path) {
+    DIR *dir;
+    struct dirent *ent;
+    int i = 0;
+
+    if ((dir = opendir(path)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_REG) {
+                i++;
+            }
+        }
+        closedir(dir);
+    } else {
+        errx(1, "count_files_in_dir: Could not open directory %s", path);
+    }
+
+    return i;
+}
--- src/Utils/file_utils.h
+++ src/Utils/file_utils.h
@@ -1,4 +1,21 @@
 #pragma once
 #include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <err.h>
 
 int file_exists(const char *filename);
+
+// list files and sub-directories in a directory
+//
+// path: path to directory
+// n: initial number of files to list, will be modified
+//    with the number of files found
+// append_path: if true, filenames will be prefixed with the path
+// returns: list of files in directory
+char **list_files(const char *path, const int n, int append_path);
+
+
+
+int count_files_in_dir(const char *path);
