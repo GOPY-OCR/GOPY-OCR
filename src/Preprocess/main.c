be0ed7b9db8e7a0e8c3b879f1ffa612b61158502
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-11 16:03:26 +0100

    Add example in the main file
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Preprocess/main.c
+++ src/Preprocess/main.c
@@ -1,11 +1,10 @@
 #include <SDL2/SDL.h>
 #include "resize.h"
+#include "brightness.h"
 
 int main(int argc, char **argv) {
     SDL_Surface *image = load_image(argv[1]);
 
-    SDL_Surface *test = new_blank_surface(image);
-    save_image(test, "put.png");
     resize(&image);
 
     save_image(image, "output.png");
