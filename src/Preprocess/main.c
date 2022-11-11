0b4503e32c26526747ca5ddb7fba4133c8a66516
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-11 16:24:28 +0100

    Add resize function for neural network special steps
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Preprocess/main.c
+++ src/Preprocess/main.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include "resize.h"
 #include "brightness.h"
+#include "split.h"
 
 int main(int argc, char **argv) {
     SDL_Surface *image = load_image(argv[1]);
@@ -9,6 +10,14 @@ int main(int argc, char **argv) {
 
     save_image(image, "output.png");
 
+    SDL_Surface *splitted[81] = {NULL};
+    split_sudoku(image, splitted);
+    neural_network_resize(splitted);
+
+    save_image(splitted[0], "test.png");
+
+    for (size_t i = 0; i < 81; i++)
+        SDL_FreeSurface(splitted[i]);
     SDL_FreeSurface(image);
 
     return 0; 
--- src/Preprocess/resize.c
+++ src/Preprocess/resize.c
@@ -23,3 +23,25 @@ void resize(SDL_Surface **base) {
     *base = dest;
 }
 
+void neural_network_resize(SDL_Surface *splitted[81]) {
+    SDL_Rect drect = { 0, 0, NN_SIZE, NN_SIZE };
+
+    for (size_t i = 0; i < 81; i++) {
+        SDL_Surface *dest = SDL_CreateRGBSurface(
+                (splitted[i])->flags,
+                NN_SIZE,
+                NN_SIZE,
+                (splitted[i])->format->BitsPerPixel,
+                (splitted[i])->format->Rmask,
+                (splitted[i])->format->Gmask,
+                (splitted[i])->format->Bmask,
+                (splitted[i])->format->Amask
+                );
+
+        if (SDL_BlitScaled(splitted[i], NULL, dest, &drect) != 0)
+            errx(EXIT_FAILURE, "%s", SDL_GetError());
+        
+        SDL_FreeSurface(splitted[i]);
+        splitted[i] = dest;
+    }
+}
