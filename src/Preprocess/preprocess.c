47cd66ea41446e4b9e3a0ae7a137ef566cf5b762
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-11 18:09:06 +0100

    Add all functions
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Preprocess/preprocess.c
+++ src/Preprocess/preprocess.c
@@ -1,6 +1,53 @@
 #include "preprocess.h"
 
+/* 
+ * this function will execute the whole preprocess. 
+ * It will execute the following steps:
+ * 1.  Load the image as a SDL_Surface
+ * 2.  Resize the image to speed up the next functions
+ * 3.  Grayscale
+ * 4.  Noise reduction + contrasts correction
+ * 5.  Binarization
+ * 6.  Interpolation des images pas droites
+ * 7.  Grid detection
+ * 8.  Automatic rotation
+ * 9.  Crop the image
+ * 10. Split the image in 81 small images
+ * 11. Resize the image to 28x28 for the neural network
+ */
 SDL_Surface **preprocess(const char *filename) {
+    // 1.  Load the image as a SDL_Surface
+    SDL_Surface *image = load_image(filename);
+
+    // 2.  Resize the image to speed up the next functions
+    resize(&image);
+
+
+    // 3.  Grayscale
+    surface_to_grayscale(image);
+
+    // 4.  Noise reduction + contrasts correction
+    correct_brightness(image);
+
+    // 5.  Binarization
+    binarize(image);
     
+    // 6.  Interpolation des images pas droites
+    
+    // 7.  Grid detection
+    Quad coords = grid_detection(image);
+    
+    // 8.  Automatic rotation
+    automatic_rotation(&image);
+    
+    // 9.  Crop the image
+    
+    // 10. Split the image in 81 small images
+    SDL_Surface **splitted = split_sudoku(image);
+    
+    // 11. Resize the image to 28x28 for the neural network
+    neural_network_resize(splitted);
+
+    return splitted;
 }
 
