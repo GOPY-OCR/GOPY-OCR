e871ca1ce0571e16740b278feb8d785acb960108
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-12 15:51:34 +0100

    Make main function call CLI for now
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/cli.h
+++ src/cli.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef CLI_H
+#define CLI_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
--- src/main.h
+++ src/main.h
@@ -1,37 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <err.h>
-#include "sdl_utils.h"
+#include <stdlib.h>
+#include "cli.h"
 
-// Grayscale part
-#include "Preprocess/grayscale.h"
-
-// Binarization part
-#include "Preprocess/binarize.h"
-
-// Brightness / contrast / noise part
-#include "Preprocess/brightness.h"
-
-// Rotation part
-#include "Preprocess/rotation.h"
-
-// Detect grid part
-#include "Preprocess/hough_transform.h"
-#include "Preprocess/flood_fill.h"
-#include "Preprocess/debug_utils.h"
-
-// Cut part
-#include "Preprocess/split.h"
-
-// Solver part
-#include "Solver/grid.h"
-#include "Solver/solver.h"
-
-// Neural Network part
-#include "NeuralNetwork/xor.h"
+int main(int argc, char *argv[]);
 
 #endif
 
