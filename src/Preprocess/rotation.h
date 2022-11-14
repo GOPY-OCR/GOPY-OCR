031bceccc00b4ace3ecf8f566987216d8fa8b521
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-14 16:58:25 +0100

    New rotation
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Preprocess/rotation.h
+++ src/Preprocess/rotation.h
@@ -1,7 +1,9 @@
 #pragma once
-#include "SDL2_rotozoom.h"
+#include <math.h>
+#include <SDL2/SDL.h>
+#include "pixel_utils.h"
 #include "grid_detection.h"
 
-SDL_Surface *manual_rot(SDL_Surface* image, double angle);
-SDL_Surface *auto_rot(SDL_Surface *image);
+void manual_rot(SDL_Surface **image, double angle);
+void automatic_rot(SDL_Surface **image);
 
