ec3c31964a87579e826206ca1d5bf7410537b57a
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-17 23:47:26 +0100

    Add Solver tests
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- tests/SolverTests/test.c
+++ tests/SolverTests/test.c
@@ -1,2 +1,156 @@
- #include <criterion/criterion.h>
- #include "test_boards.c"
+#include <criterion/criterion.h>
+#include "solver.h"
+#include "grid.h"
+
+int is_the_same(int *board, char *filename) {
+    int *ref = load_grid(filename);
+
+    for (size_t i = 0; i < BOARDSIZE * BOARDSIZE; i++)
+        if (board[i] != ref[i]) {
+            save_grid(board, "modified_grid");
+            return 0;
+        }
+
+    free(ref);
+
+    return 1;
+}
+
+
+Test(IsLineValid, valid_lines) {
+    int *board = load_grid("valid_lines");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(IsLineValid(board, i));
+
+    cr_failure(is_the_same(board, "valid_lines"));
+
+    free(board);
+}
+
+Test(IsLineValid, wrong_lines) {
+    int *board = load_grid("wrong_lines");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(!IsLineValid(board, i));
+
+    cr_failure(is_the_same(board, "wrong_lines"));
+
+    free(board);
+}
+
+Test(IsColumnValid, valid_columns) {
+    int *board = load_grid("valid_columns");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(IsColumnValid(board, i));
+
+    cr_failure(is_the_same(board, "valid_columns"));
+
+    free(board);
+}
+
+Test(IsColumnValid, wrong_columns) {
+    int *board = load_grid("wrong_columns");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(!IsColumnValid(board, i));
+
+    cr_failure(is_the_same(board, "wrong_columns"));
+
+    free(board);
+}
+
+Test(IsSquareValid, valid_square) {
+    int *board = load_grid("valid_square");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(IsSquareValid(board, i));
+
+    cr_failure(is_the_same(board, "valid_square"));
+
+    free(board);
+}
+
+Test(IsSquareValid, wrong_square) {
+    int *board = load_grid("wrong_square");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(!IsSquareValid(board, i));
+
+    cr_failure(is_the_same(board, "wrong_square"));
+
+    free(board);
+}
+
+Test(IsBoardValid, valid_board) {
+    int *board = load_grid("valid_board");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(IsBoardValid(board, i));
+
+    cr_failure(is_the_same(board, "valid_board"));
+
+    free(board);
+}
+
+Test(IsBoardValid, wrong_board) {
+    int *board = load_grid("wrong_board");
+
+    for (size_t i = 0; i < BOARDSIZE; i++)
+        cr_expect(!IsBoardValid(board, i));
+
+    cr_failure(is_the_same(board, "wrong_board"));
+
+    free(board);
+}
+
+Test(IsSolved, solved_board) {
+    int *board = load_grid("solved_board");
+
+    cr_expect(IsSolved(board));
+
+    cr_failure(is_the_same(board, "solved_board"));
+
+    free(board);
+}
+
+Test(IsSolved, not_solved_valid_board) {
+    int *board = load_grid("not_solved_valid_board");
+
+    cr_expect(!IsSolved(board));
+
+    cr_failure(is_the_same(board, "not_solved_valid_board"));
+
+    free(board);
+}
+
+Test(IsSolved, not_solved_not_valid_board) {
+    int *board = load_grid("not_solved_not_valid_board");
+
+    cr_expect(!IsSolved(board));
+
+    cr_failure(is_the_same(board, "not_solved_not_valid_board"));
+
+    free(board);
+}
+
+Test(Solve, solvable) {
+    int *board = load_grid("solvable");
+
+    cr_expect(Solve(board));
+
+    cr_failure(IsSolved(board));
+
+    free(board);
+}
+
+Test(Solve, not_solvable) {
+    int *board = load_grid("not_solvable");
+
+    cr_expect(!Solve(board));
+
+    cr_failure(is_the_same(board, "not_solvable"));
+
+    free(board);
+}
