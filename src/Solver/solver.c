0a4267a10de79fdbdf98eea6ac1f516e32a266fb
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-17 22:43:53 +0100

    Solve solver conflicts

--- src/Solver/solver.c
+++ src/Solver/solver.c
@@ -2,15 +2,15 @@
 
 
 //_IsLineValid checks if the lign is valid
-int IsLineValid(int board[], size_t line){
+int IsLineValid(int board[], size_t line) {
     int numbers[BOARDSIZE] = {0};
 
-    for (size_t column = 0; (int)column < BOARDSIZE; column ++){
-        if (board[line * BOARDSIZE + column] != 0){
-            if (numbers[board[line * BOARDSIZE + column] - 1] == 1){
+    for (size_t column = 0; column < BOARDSIZE; column++) {
+        if (board[line * BOARDSIZE + column] != 0) {
+            if (numbers[board[line * BOARDSIZE + column] - 1] == 1) {
                 return 0;
             }
-            else{ 
+            else { 
                 numbers[board[line * BOARDSIZE + column] - 1] = 1;
             }
         }
@@ -20,34 +20,34 @@ int IsLineValid(int board[], size_t line){
 }
 
 //_IsColumnValid checks if the column is valid
-int IsColumnValid(int board[], size_t column){
+int IsColumnValid(int board[], size_t column) {
     int numbers[BOARDSIZE] = {0};
 
-    for (size_t line = 0; (int)line < BOARDSIZE; line ++){
-        if (board[line * BOARDSIZE + column] != 0){
-            if (numbers[board[line * BOARDSIZE + column] - 1] == 1){
+    for (size_t line = 0; line < BOARDSIZE; line++) {
+        if (board[line * BOARDSIZE + column] != 0) {
+            if (numbers[board[line * BOARDSIZE + column] - 1] == 1) {
                 return 0;
             }
-            else{
+            else {
                 numbers[board[line * BOARDSIZE + column] - 1] = 1;
             }
         }         
     }
 
-     return 1;
+    return 1;
  }
 
 //IsSquareValid checks if the Square is valid
-int IsSquareValid(int board[], int line, int column){
+int IsSquareValid(int board[], size_t line, size_t column) {
     int numbers[BOARDSIZE] = {0};
 
-    for (size_t i = line; (int)i < line + 3; i ++){
-        for (size_t j = column; (int)j < column + 3; j ++){
-            if (board[i*BOARDSIZE+j] != 0){
-                 if (numbers[board[i * BOARDSIZE + j] - 1] == 1){
+    for (size_t i = line; i < line + 3; i ++) {
+        for (size_t j = column; j < column + 3; j ++) {
+            if (board[i * BOARDSIZE + j] != 0) {
+                 if (numbers[board[i * BOARDSIZE + j] - 1] == 1) {
                      return 0;
                  }
-                 else{
+                 else {
                      numbers[board[i * BOARDSIZE + j] - 1] = 1;
                  }
              }        
@@ -58,14 +58,14 @@ int IsSquareValid(int board[], int line, int column){
 }
 
 //IsBoardValid checks if the board is valid
-int IsBoardValid(int board[]){
-    for (size_t i = 0; (int)i < BOARDSIZE; i ++){
+int IsBoardValid(int board[]) {
+    for (size_t i = 0; i < BOARDSIZE; i++) {
         if (!IsColumnValid(board, i) || !IsLineValid(board, i))
             return 0;
     }
 
-    for (size_t line = 0; (int)line <= 6; line += 3){
-        for (size_t column = 0; (int)column <= 6; column += 3){
+    for (size_t line = 0; line <= 6; line += 3) {
+        for (size_t column = 0; column <= 6; column += 3) {
             if (!IsSquareValid(board, line, column))
                 return 0;
         }
@@ -75,11 +75,11 @@ int IsBoardValid(int board[]){
 }
 
 //IsSolved checks if the board is solved
-int IsSolved(int board[]){
-    if (IsBoardValid(board)){
-        for (size_t i = 0; (int)i < BOARDSIZE; i ++){
-            for (size_t j = 0; (int)j < BOARDSIZE; j ++){
-                if (board[i * BOARDSIZE + j] == '0'){
+int IsSolved(int board[]) {
+    if (IsBoardValid(board)) {
+        for (size_t i = 0; i < BOARDSIZE; i++) {
+            for (size_t j = 0; j < BOARDSIZE; j++) {
+                if (board[i * BOARDSIZE + j] == 0) {
                     return 0;
                 }
             }
@@ -89,35 +89,24 @@ int IsSolved(int board[]){
     return 1;
 }
 
-int zero(int list[BOARDSIZE]){
-    size_t i = 0;
-    while ((int)i < BOARDSIZE && list[i] == 0){
-        i ++;
-    }
-    return ((int)i == BOARDSIZE) ? 1 : 0;
-}
-
 
 //Solve solves the board
-int Solve(int board[]){
+int Solve(int board[]) {
     
-    for (size_t i = 0; (int)i < BOARDSIZE; i ++){
-        for (size_t j = 0; (int)j < BOARDSIZE; j ++){
-            if (board[i * BOARDSIZE + j] == 0){
+    for (size_t i = 0; i < BOARDSIZE; i++) {
+        for (size_t j = 0; j < BOARDSIZE; j++) {
+            if (board[i * BOARDSIZE + j] == 0) {
                 int num = 1;
-                for (; num <= BOARDSIZE; num ++){
+                for (; num <= BOARDSIZE; num++) {
                     board[i * BOARDSIZE + j] = num;
-                    if (IsLineValid(board, i) && IsColumnValid(board, j) && IsSquareValid(board, i, j)){
-                        if (Solve(board) == 1){
+                    if (IsLineValid(board, i) && 
+                        IsColumnValid(board, j) && 
+                        IsSquareValid(board, i, j) &&
+                        Solve(board) == 1)
                             break;
-                        }
-                        else{
-                            continue;
-                        }
-                    }
                 }
  
-                if (num == BOARDSIZE + 1){
+                if (num == BOARDSIZE + 1) {
                     board[i * BOARDSIZE + j] = 0;
                     return 0;
                 }
@@ -125,7 +114,6 @@ int Solve(int board[]){
         }
     }
 
-    
     return 1;
 }
 
--- src/Solver/solver.h
+++ src/Solver/solver.h
@@ -10,12 +10,12 @@
 #define BOARDSIZE 9
 
 //PROTOTYPES
-int IsLineValid(int board[BOARDSIZE], size_t line);
-int IsColumnValid(int board[BOARDSIZE], size_t column);
-int IsSquareValid(int board[BOARDSIZE], int line, int column);
-int IsBoardValid(int board[BOARDSIZE]);
-int IsSolved(int board[BOARDSIZE]);
-int Solve(int board[BOARDSIZE]);
+int IsLineValid(int board[], size_t line);
+int IsColumnValid(int board[], size_t column);
+int IsSquareValid(int board[], size_t line, size_t column);
+int IsBoardValid(int board[]);
+int IsSolved(int board[]);
+int Solve(int board[]);
 
 
 #endif
