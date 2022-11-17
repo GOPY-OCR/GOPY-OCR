63f4aad1cd3c859c072cd32ca01b88e93ad86ae5
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-11-17 10:34:02 +0100

    Add draw grid function
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/Postprocess/create_image.c
+++ src/Postprocess/create_image.c
@@ -1,1 +1,38 @@
 #include "create_image.h"
+
+int is_initial_number(int x, int y, int *grid) {
+    return init[x * 9 + y] == 0;
+}
+
+void draw_grid(SDL_Surface *res, int nb_cases) {
+    SDL_FillRect(res, NULL, GRID_COLOR);
+    
+    SDL_Rect *fills = calloc(BOARDSIZE * BOARDSIZE, sizeof(SDL_Rect));
+
+    for(size_t i = 0; i < BOARDSIZE * BOARDSIZE; i++) {
+        fills->x = (i % BOARDSIZE) * CELL_SIZE;
+        fills->y = (i / BOARDSIZE) * CELL_SIZE;
+        fills->w = CELL_SIZE;
+        fills->h = CELL_SIZE;
+    }
+
+    SDL_FillRects(res, fills, BOARDSIZE * BOARDSIZE, BACK_COLOR);
+}
+
+void add_number(SDL_Surface *res, int x, int y, int number) {
+    TTF_Font *font = TTF_OpenFont(FONT, 24);
+    SDL_Color color = is_initial_number(x, y, grid) ? OLD_NUMBER_COLOR : NEW_NUMBER_COLOR;
+
+    char msg[] = number == 0 ? " " : { number + 48, 0 };
+    SDL_Surface* letter = TTF_RenderText_Solid(font, msg, color);
+
+    SDL_Rect msg_rect;
+    msg_rect.x = x * CELL_SIZE;
+    msg_rect.y = y * CELL_SIZE;
+    msg_rect.w = CELL_SIZE;
+    msg_rect.h = CELL_SIZE;
+
+    SDL_BlitSurface(letter, NULL, res, &msg_rect);
+
+    SDL_FreeSurface(letter);
+}
--- src/Postprocess/create_image.h
+++ src/Postprocess/create_image.h
@@ -2,8 +2,20 @@
 #define POST_CREATE_IMAGE_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#define OLD_NUMBER_COLOR {0, 0, 0}
+#define NEW_NUMBER_COLOR {0, 0, 255}
+#define GRID_COLOR 0xFFFFFFFF
+#define BACK_COLOR 0
+#define CELL_SIZE 100
+#define FONT "ArchivoNarrow-Regular.ttf"
 
-int is_initial_number(int x, int y);
+#ifndef BOARDSIZE
+    #define BOARDSIZE 9
+#endif
 
+int is_initial_number(int x, int y, int *grid);
+void draw_grid(SDL_Surface *res, int nb_cases);
+void add_number(SDL_Surface *res, int x, int y, int number);
 
 #endif
