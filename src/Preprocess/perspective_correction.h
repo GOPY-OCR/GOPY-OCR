702d3b95478c101529ce224a54a67ab170d71276
oscar <oscar.le-dauphin@epita.fr>
2022-11-25 18:15:30 +0100

    removed unused include
    Signed-off-by: oscar <oscar.le-dauphin@epita.fr>

--- src/Preprocess/perspective_correction.h
+++ src/Preprocess/perspective_correction.h
@@ -3,9 +3,7 @@
 #include <assert.h>
 #include "geometry.h"
 #include "pixel_utils.h"
-#include "matrice.h"
 
-// this is called a bilinear quadrilateral transformation
 SDL_Surface* perspective_correction(SDL_Surface *src, Quad *grid);
 
 Point get_perspective_point(Point src, Quad *grid, int width, int height);
