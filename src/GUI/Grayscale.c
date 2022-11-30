e30b23341ba026728c374144b2a62d7d4ec29ba0
Pierre Wagniart <pierre.wagniart@epita.fr>
2022-11-30 15:28:54 +0100

    first try

---
+++ src/GUI/Grayscale.c
@@ -0,0 +1,17 @@
+//#include "Grayscale.h"
+
+//GtkWidget *CancelButton1;
+//GtkWidget *ContinueButton1;
+
+//void page_2_init(GtkBuilder *builder){
+   //CancelButton1 = GTK_WIDGET(gtk_builder_get_object(builder, "Cancel1"));
+  // ContinueButton1 = GTK_WIDGET(gtk_builder_get_object(builder, "Continue1"));
+
+   // g_signal_connect(CancelButton1, "clicked", G_CALLBACK(on_CancelButton1_clicked), NULL);
+
+//}
+
+//G_MODULE_EXPORT void on_CancelButton1_clicked(GtkButton *button, gpointer user_data){
+//    clear_image();
+//    prev_page();
+//}
