31db481cf9b922f58a20443df6f17fd6628018fc
YnessK <97630948+YnessK@users.noreply.github.com>
2022-12-03 13:00:37 +0100

    save page

--- src/GUI/save.c
+++ src/GUI/save.c
@@ -1,11 +1,11 @@
-#include "anima.h"
+#include "save.h"
 
 
 GtkWidget *PreviousButton;
 GtkWidget *SaveButton;
 
 
-void anima_init(GtkBuilder *builder)
+void save_init(GtkBuilder *builder)
 {
     PreviousButton = GTK_WIDGET(gtk_builder_get_object(builder, "Previous"));
     SaveButton = GTK_WIDGET(gtk_builder_get_object(builder, "Save"));
@@ -15,14 +15,3 @@ void anima_init(GtkBuilder *builder)
 }
 
 
-G_MODULE_EXPORT void on_PreviousButton_clicked(GtkButton *button, gpointer user_data)
-{
-    clear_image();
-    prev_page();
-}
-
-
-G_MODULE_EXPORT void on_SaveButton_clicked(GtkButton *button, gpointer user_data)
-{
-    next_page();
-}
