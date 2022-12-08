f7fd8f236b8c5cfc19ad133cf14ba8a180205715
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-12-08 22:30:12 +0100

    Repare anima_steps struct
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/GUI/anima_steps.h
+++ src/GUI/anima_steps.h
@@ -2,16 +2,17 @@
 #define ANIMA_STEPS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 
 #define NB_STEPS 10
 #define NB_PRE_STEPS 8
-typedef Anima_Steps {
+typedef struct Anima_Steps {
     int nb_steps; // 11 for now
     int nb_pre_steps; // 
     int cur_step; // 0-7 for preprocess steps
                   // 8 for detected grid
                   // 9 for postprocess step
-    SDL_Surface **preprocess_states; // 8 Steps (grid_detection with red lines +
+    SDL_Surface **prep; // 8 Steps (grid_detection with red lines +
                                      // no splitted grid bc it is useless)
     int *detected;
     int *solved;
