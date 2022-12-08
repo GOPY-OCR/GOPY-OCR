0b06cf3d55572704efe423dfa701603584d5f906
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-12-08 03:23:57 +0100

    CLI done
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/main.h
+++ src/main.h
@@ -1,50 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <err.h>
-#include "sdl_utils.h"
+#include <stdlib.h>
+#include "CLI/cli.h"
+//#include "GUI/GUI.h"
 
-// Grayscale part
-#include "Preprocess/grayscale.h"
-
-// Binarization part
-#include "Preprocess/binarize.h"
-
-// Brightness / contrast / noise part
-#include "Preprocess/brightness.h"
-
-// Rotation part
-#include "Preprocess/rotation.h"
-
-// Detect grid part
-#include "Preprocess/hough_transform.h"
-#include "Preprocess/flood_fill.h"
-#include "Preprocess/debug_utils.h"
-
-// Perspective correction part
-#include "Preprocess/perspective_correction.h"
-
-// Cut part
-#include "Preprocess/split.h"
-
-// Preprocess
-#include "Preprocess/preprocess.h"
-#include "Preprocess/params.h"
-
-// Solver part
-#include "Solver/grid.h"
-#include "Solver/solver.h"
-
-// Neural Network part
-#include "NeuralNetwork/xor.h"
-#include "NeuralNetwork/neural_network.h"
-#include "NeuralNetwork/digit_recognition.h"
-
-// Postprocess
-#include "Postprocess/postprocess.h"
-#include "Solver/grid.h"
 
 #endif
 
