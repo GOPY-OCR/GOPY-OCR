620138932b5dfdbbf50608af77dfcb045af31fd4
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2022-12-08 10:57:22 +0100

    started implementing adaptative threshold

--- src/Preprocess/binarize.c
+++ src/Preprocess/binarize.c
@@ -7,3 +7,35 @@ void binarize(SDL_Surface *base, int threshold) {
         pixels[i] = ((Uint8) pixels[i]) >= threshold ? 0 : 0xFFFFFF;
 }
 
+// algorithm from https://www.researchgate.net/publication/220494200_Adaptive_Thresholding_using_the_Integral_Image
+void adaptative(SDL_Surface **base){
+    SDL_Surface *src = *base;
+    SDL_Surface *dst = SDL_CreateRGBSurface(SDL_SWSURFACE, src->w, src->h, 32, 0, 0, 0, 0);
+    matrice *integral = matrice_new(src->w, src->h);
+    
+
+    for (int x = 0; x < src->w; x++){
+        int sum = 0;
+        for (int y = 0; y < src->h; y++){
+            sum += getpixelgray(src, x, y);
+            
+            assert(sum > 0);
+            
+            if(x == 0){
+                matrice_set(integral, x, y, (double) sum);
+            }
+            else {
+                double upper = matrice_get(integral, x - 1, y);
+                matrice_set(integral, x, y, upper + (double) sum);
+            }
+        }
+    }
+    
+
+    for (int x = 0; x < src->w; x++){
+        for (int y = 0; y < src->h; y++){
+            int x1 = i
+        }
+    }
+}
+
--- src/Preprocess/binarize.h
+++ src/Preprocess/binarize.h
@@ -3,8 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "matrice.h"
 
 void binarize(SDL_Surface *base, int threshold);
+
+void adaptative(SDL_Surface **base);
 
 #endif
 
