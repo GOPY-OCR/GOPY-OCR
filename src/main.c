97b375483e32a42d25b0dfaeecb37b32d3c44bf6
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2022-12-08 09:48:56 +0100

    Add GUI to the main exe
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 int main(int argc, char* argv[]) {
-    /*
-    if (argc != 1)
+    if (argc == 1)
         return GUI(argc, argv);
 
     else
-    */
         return CLI(argc, argv);
 }
 
--- src/main.h
+++ src/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "CLI/cli.h"
-//#include "GUI/GUI.h"
+#include "GUI/GUI.h"
 
 
 #endif
